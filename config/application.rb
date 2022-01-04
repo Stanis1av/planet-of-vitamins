10ee1269f7549e7b86e62b5cf52f97d723f8023a
Stanis1av <krasavin.stanislaus@gmail.com>
2022-01-05 03:58:56 +0400

    Updating devise views and adding styles to them

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,19 @@ module PlanetOfVitamins
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Remove HTML <div> blocks on notifications
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
+
+    # Whitelist locales avialable (English, Russian, Deutsche)
+    # config.i18n.available_locales = [:en, :ru, :de]
+    # Set default locale :en (English)
+    config.i18n.default_locale = :ru
+
+    # The default locale loading mechanism in Rails does not load locale files
+    # in nested dictionaries, like we have here. So, for this to work, we must
+    # explicitly tell Rails to look further:
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
   end
 end
