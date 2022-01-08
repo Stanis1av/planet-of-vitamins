8aeb811fb7d7a9cb7405c20da0fcb89f95b071f7
Stanis1av <krasavin.stanislaus@gmail.com>
2022-01-08 09:06:47 +0400

    initialization rails 6 app

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module PlanetOfVitamins
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,19 +18,5 @@ module PlanetOfVitamins
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Remove HTML <div> blocks on notifications
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
-
-    # Whitelist locales avialable (English, Russian, Deutsche)
-    # config.i18n.available_locales = [:en, :ru, :de]
-    # Set default locale :en (English)
-    config.i18n.default_locale = :ru
-
-    # The default locale loading mechanism in Rails does not load locale files
-    # in nested dictionaries, like we have here. So, for this to work, we must
-    # explicitly tell Rails to look further:
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
   end
 end
--- config/initializers/send_in_blue.rb
+++
@@ -1,3 +0,0 @@
-SibApiV3Sdk.configure do |config|
-  config.api_key['api-key'] = ENV["send_in_blue_api_key"]
-end
